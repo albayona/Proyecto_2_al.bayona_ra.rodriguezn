7fdc62e53a9155f8df01d0278c7fbc15a02bbf45
Andres Bayona <al.bayona@uniandes.edu.co>
2019-10-29 17:01:50 -0500

    View

--- src/view/MVCView.java
+++ src/view/MVCView.java
@@ -2,23 +2,32 @@ package view;
 
 public class MVCView
 {
-	    /**
-	     * Metodo constructor
-	     */
-	    public MVCView()
-	    {
-	    	
-	    }
-	    
-		public void printMenu()
-		{
-			System.out.println("1. Cargar los datos.");
+	/**
+	 * Metodo constructor
+	 */
+	public MVCView()
+	{
 
-		}
+	}
 
-		public void printMessage(String mensaje) {
+	public void printMenu()
+	{
+		System.out.println("1. Cargar los datos.");
+		System.out.println("A1. Requerimiento 1 de la parte A");
+		System.out.println("A2. Requerimiento 2 de la parte A");
+		System.out.println("A3. Requerimiento 3 de la parte A");
+		System.out.println("B1. Requerimiento 1 de la parte B");
+		System.out.println("B2. Requerimiento 2 de la parte B");
+		System.out.println("B3. Requerimiento 3 de la parte B");
+		System.out.println("C1. Requerimiento 1 de la parte C");
+		System.out.println("C2. Requerimiento 2 de la parte C");
+		System.out.println("C3. Requerimiento 3 de la parte C");
 
-			System.out.println(mensaje);
-		}		
-		
+	}
+
+	public void printMessage(String mensaje) {
+
+		System.out.println(mensaje);
+	}
+
 }
